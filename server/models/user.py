366c7af9b7f5ccabb901f6bb696613059cf0a913
margaiwangara <margaiwangara@gmail.com>
2023-09-04 16:02:54 +0300

    feat: password reset

--- server/models/user.py
+++ server/models/user.py
@@ -2,7 +2,7 @@ from database import Base
 from sqlalchemy.sql import func
 from sqlalchemy import Column, Integer, String, TIMESTAMP, Boolean
 from utils import constants
-from pydantic import BaseModel, EmailStr, Field
+from pydantic import BaseModel, EmailStr, Field, UUID4
 
 
 class User(Base):
@@ -13,7 +13,7 @@ class User(Base):
     email = Column(String(50), nullable=False, unique=True, index=True)
     password = Column(String(255), nullable=False)
     is_confirmed = Column(Boolean, default=True)
-    role = Column(String(10), default=constants.USER_ROLE)
+    role = Column(String(10), default=constants.USER_ROLE)  # roles
     created_at = Column(TIMESTAMP(timezone=True), server_default=func.now())
 
 
@@ -29,12 +29,16 @@ class ForgotPasswordRequest(BaseModel):
 
 
 class ResetPasswordRequest(BaseModel):
+    email: EmailStr
+    code: UUID4
     password: str = Field(min_length=8)
     confirm_password: str = Field(min_length=8)
 
     class Config:
         json_schema_extra = {
             "example": {
+                "email": "johndoe@syntaxsaga.dev",
+                "code": "some random code value",
                 "password": "Password123",
                 "confirm_password": "Password123"
             }
--- server/routers/auth.py
+++ server/routers/auth.py
@@ -5,9 +5,10 @@ from starlette import status
 from utils.email import send_email
 from config import settings
 from uuid import uuid4
-from utils import constants
+from lib.auth import check_email_exists
 
 import redis
+import json
 
 router = APIRouter(
     tags=["Authentication and Authorization"],
@@ -33,29 +34,50 @@ def login_user():
 @router.post("/forgot-password", status_code=status.HTTP_204_NO_CONTENT)
 # takes in an email address and send reset password email
 def forgot_user_password(input: ForgotPasswordRequest, db: DB_DEPENDENCY, background_tasks: BackgroundTasks):
-    # check if email exists
-    email_exists = db.query(User).filter(User.email == input.email).first()
-
-    if email_exists is None:
-        raise HTTPException(404, "Email address does not exist")
+    email_exists = check_email_exists(input.email, db)
 
     # generate reset code and store in Redis
     reset_code = uuid4()
-    r.set(constants.PASSWORD_RESET_CODE, str(reset_code), ex=60*60*24)
+    r.set(email_exists.email, str(reset_code), ex=60*60*24)
 
     reset_link = f"{settings.CLIENT_PATH}/reset-password?code={reset_code}"
     # send email to user in the background
     background_tasks.add_task(
         send_email,
-        input.email,
+        email_exists.email,
         "Reset Your Password",
         f"Click this link to reset your password: <a href=\"{reset_link}\">{reset_link}</a>. The link will expire in 24hrs."
     )
 
 
-@router.post("/reset-password")
-def reset_user_password():  # takes in a password and confirm_password and resets user email
-    return {"message": "Reset User Password"}
+@router.post("/reset-password", status_code=status.HTTP_204_NO_CONTENT)
+# takes in a password and confirm_password and resets user email
+def reset_user_password(input: ResetPasswordRequest, db: DB_DEPENDENCY, background_tasks: BackgroundTasks):
+    # check if email exists
+    email_exists = check_email_exists(input.email, db)
 
-# s2.domainkey.u24068541.wl042.sendgrid.net.
-# s1.domainkey.u24068541.wl042.sendgrid.net.
+    # check if code exists or is invalid
+    reset_code = r.get(email_exists.email)
+
+    if reset_code is None or reset_code.decode() != str(input.code):
+        raise HTTPException(400, "Invalid password reset code")
+
+    # delete entry from redis
+    r.delete(email_exists.email)
+
+    # check if passwords match
+    if input.password != input.confirm_password:
+        raise HTTPException(400, "Passwords must match")
+
+    # reset password
+    email_exists.password = input.password
+    db.add(email_exists)
+    db.commit()
+
+    # send confirmation email
+    background_tasks.add_task(
+        send_email,
+        email_exists.email,
+        "Password Reset Successful",
+        "Your password has been reset successfully"
+    )
